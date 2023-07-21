109a0766fc44930882cfa2161c64eac9ab992e7a
WAITUTE <109899422+WAITUTE@users.noreply.github.com>
2023-07-21 22:47:15 +0300

    Add files via upload
    For recording work done in hours. and talbing the result for earch worker

---
+++ Form1.Designer.cs
@@ -0,0 +1,341 @@
+﻿namespace AppManagement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBoxNational = new System.Windows.Forms.TextBox();
+            this.txtBoxFirst = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBoxOther = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBoxId = new System.Windows.Forms.ComboBox();
+            this.txtBoxWork = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtBoxCompe = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.txtBoxSearch = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnView = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "National Id";
+            // 
+            // txtBoxNational
+            // 
+            this.txtBoxNational.Location = new System.Drawing.Point(143, 32);
+            this.txtBoxNational.Name = "txtBoxNational";
+            this.txtBoxNational.Size = new System.Drawing.Size(134, 20);
+            this.txtBoxNational.TabIndex = 1;
+            // 
+            // txtBoxFirst
+            // 
+            this.txtBoxFirst.Location = new System.Drawing.Point(143, 71);
+            this.txtBoxFirst.Name = "txtBoxFirst";
+            this.txtBoxFirst.Size = new System.Drawing.Size(134, 20);
+            this.txtBoxFirst.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "First Name";
+            // 
+            // txtBoxOther
+            // 
+            this.txtBoxOther.Location = new System.Drawing.Point(143, 109);
+            this.txtBoxOther.Name = "txtBoxOther";
+            this.txtBoxOther.Size = new System.Drawing.Size(134, 20);
+            this.txtBoxOther.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Other Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Gender";
+            // 
+            // comboBoxId
+            // 
+            this.comboBoxId.FormattingEnabled = true;
+            this.comboBoxId.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.comboBoxId.Location = new System.Drawing.Point(143, 152);
+            this.comboBoxId.Name = "comboBoxId";
+            this.comboBoxId.Size = new System.Drawing.Size(134, 21);
+            this.comboBoxId.TabIndex = 8;
+            // 
+            // txtBoxWork
+            // 
+            this.txtBoxWork.Location = new System.Drawing.Point(143, 192);
+            this.txtBoxWork.Name = "txtBoxWork";
+            this.txtBoxWork.Size = new System.Drawing.Size(134, 20);
+            this.txtBoxWork.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(39, 199);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Workhours done";
+            // 
+            // txtBoxCompe
+            // 
+            this.txtBoxCompe.Location = new System.Drawing.Point(143, 231);
+            this.txtBoxCompe.Name = "txtBoxCompe";
+            this.txtBoxCompe.Size = new System.Drawing.Size(134, 20);
+            this.txtBoxCompe.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(39, 238);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(74, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Compensation";
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(0, 71);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(75, 23);
+            this.btnNew.TabIndex = 13;
+            this.btnNew.Text = "New";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(121, 19);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 14;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(0, 19);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 15;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(543, 111);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 16;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(691, 109);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 17;
+            this.btnUpdate.Text = "update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // txtBoxSearch
+            // 
+            this.txtBoxSearch.Location = new System.Drawing.Point(376, 44);
+            this.txtBoxSearch.Name = "txtBoxSearch";
+            this.txtBoxSearch.Size = new System.Drawing.Size(134, 20);
+            this.txtBoxSearch.TabIndex = 19;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(353, 23);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(168, 18);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Search by National Id";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(543, 44);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(85, 20);
+            this.btnSearch.TabIndex = 20;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(691, 44);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 21;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(295, 199);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(554, 220);
+            this.dataGridView1.TabIndex = 22;
+            this.dataGridView1.CellMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseDoubleClick);
+            this.dataGridView1.RowPrePaint += new System.Windows.Forms.DataGridViewRowPrePaintEventHandler(this.dataGridView1_RowPrePaint);
+            // 
+            // btnView
+            // 
+            this.btnView.Location = new System.Drawing.Point(121, 60);
+            this.btnView.Name = "btnView";
+            this.btnView.Size = new System.Drawing.Size(75, 23);
+            this.btnView.TabIndex = 23;
+            this.btnView.Text = "View";
+            this.btnView.UseVisualStyleBackColor = true;
+            this.btnView.Click += new System.EventHandler(this.btnView_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnCancel);
+            this.groupBox1.Controls.Add(this.btnView);
+            this.groupBox1.Controls.Add(this.btnNew);
+            this.groupBox1.Controls.Add(this.btnSave);
+            this.groupBox1.Location = new System.Drawing.Point(12, 282);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(213, 100);
+            this.groupBox1.TabIndex = 24;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "service";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(861, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtBoxSearch);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.txtBoxCompe);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtBoxWork);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBoxId);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtBoxOther);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtBoxFirst);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBoxNational);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "University Management tool";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBoxNational;
+        private System.Windows.Forms.TextBox txtBoxFirst;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBoxOther;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBoxId;
+        private System.Windows.Forms.TextBox txtBoxWork;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtBoxCompe;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.TextBox txtBoxSearch;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnView;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace AppManagement
+{
+    public partial class Form1 : Form
+    {
+        OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Admin\OneDrive\Documents\10012884\Database51.accdb");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (comboBoxId.SelectedItem == null)
+            {
+                MessageBox.Show("Please select an item from the dropdown");
+                return;  
+            }
+
+            double compensations = double.Parse(txtBoxWork.Text) * 300;
+            txtBoxCompe.Text = compensations.ToString();
+            string selecteed = comboBoxId.SelectedItem.ToString();
+            DateTime currentDate = DateTime.Now;
+            string dateString = currentDate.ToString("MM/dd/yyyy");
+            
+            MessageBox.Show("Record submite", "good!");
+            using (OleDbCommand cmd = con.CreateCommand())
+            {
+                con.Open();
+                cmd.Connection = con;
+                cmd.CommandText = "Insert into Table1 ([National Id], [First Name], [Other Name], [Gender], [Work Hours], [Compensation], [Date]) Values('" + txtBoxNational.Text + "','" + txtBoxFirst.Text + "','" + txtBoxOther.Text + "','" + selecteed + "','" + txtBoxWork.Text + "','" + compensations + "','" + dateString + "')";
+                cmd.Parameters.Add(new OleDbParameter("national", txtBoxNational.Text));
+                cmd.Parameters.Add(new OleDbParameter("first", txtBoxFirst.Text));
+                cmd.Parameters.Add(new OleDbParameter("other", txtBoxOther.Text));
+                cmd.Parameters.Add(new OleDbParameter("selected", selecteed));
+                cmd.Parameters.Add(new OleDbParameter("work", txtBoxWork.Text));
+                cmd.Parameters.Add(new OleDbParameter("compensations", compensations));
+                cmd.Parameters.Add(new OleDbParameter("Date", dateString));
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+            MessageBox.Show("Record submitted", "Good!");
+        }
+        private void loaddata()
+        {
+            string connString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Admin\OneDrive\Documents\10012884\Database51.accdb";
+            DataTable dataTableRes = new DataTable();
+
+            using (OleDbConnection conn = new OleDbConnection(connString))
+            {
+                OleDbCommand cmd = new OleDbCommand("SELECT * FROM Table1", conn);
+
+                conn.Open();
+
+                OleDbDataAdapter adapter = new OleDbDataAdapter(cmd);
+
+                adapter.Fill(dataTableRes);
+            }
+
+            dataGridView1.DataSource = dataTableRes;
+            dataGridView1.RowsDefaultCellStyle.BackColor = Color.White;
+            dataGridView1.AlternatingRowsDefaultCellStyle.BackColor = Color.FromArgb(0, 255, 0);
+
+        }
+
+        private void btnView_Click(object sender, EventArgs e)
+        {
+            loaddata();
+            dataGridView1.Sort(dataGridView1.Columns["National Id"], ListSortDirection.Ascending);
+
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            string connString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Admin\OneDrive\Documents\10012884\Database51.accdb";
+            if (dataGridView1.SelectedCells.Count > 0)
+            {
+
+                if (MessageBox.Show(string.Format("Do you want to delete Table1 registration: {0}?", txtBoxNational.Text.ToString()), "Confirmation", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    using (OleDbConnection conn = new OleDbConnection(connString))
+                    {
+                        using (OleDbCommand cmd = new OleDbCommand("DELETE FROM Table1  WHERE [National Id]=@national", conn))
+                        {
+                            cmd.CommandType = CommandType.Text;
+
+                            cmd.Parameters.AddWithValue("@national", txtBoxNational.Text);
+
+                            conn.Open();
+                            cmd.ExecuteNonQuery();
+                            conn.Close();
+                            MessageBox.Show("record deleted successfully");
+                            loaddata();
+                        }
+                    }
+
+
+                }
+            }
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string connString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Admin\OneDrive\Documents\10012884\Database51.accdb";
+            DateTime currentDate = DateTime.Now;
+            string dateString = currentDate.ToString("MM/dd/yyyy");
+            string selecteed = comboBoxId.SelectedItem.ToString();
+            double compensations = double.Parse(txtBoxWork.Text) * 300;
+            string compe = compensations.ToString();
+            if (dataGridView1.SelectedCells.Count > 0)
+            {
+
+                if (MessageBox.Show(string.Format("Do you want to update student registration: {0}?", txtBoxNational.Text.ToString()), "Confirmation", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    using (OleDbConnection conn = new OleDbConnection(connString))
+                    {
+                        using (OleDbCommand cmd = new OleDbCommand("update Table1 set [National Id]=@national, [First Name]=@first, [Other Name]=@other, [Gender]=@gender, [Work Hours]=@work, [Compensation]=@compensations, [Date]=@date WHERE [National Id] = @national", conn))
+                        {
+
+                            cmd.CommandType = CommandType.Text;
+
+                            cmd.Parameters.AddWithValue("@national", txtBoxNational.Text);
+                            cmd.Parameters.AddWithValue("@first", txtBoxFirst.Text);
+                            cmd.Parameters.AddWithValue("@other", txtBoxOther.Text);
+                            cmd.Parameters.AddWithValue("@gender", selecteed);
+                            cmd.Parameters.AddWithValue("@work", txtBoxWork.Text);
+                            cmd.Parameters.AddWithValue("@compensations", compe);
+                            cmd.Parameters.AddWithValue("@date", dateString);
+                            conn.Open();
+                            cmd.ExecuteNonQuery();
+                            conn.Close();
+                            MessageBox.Show("record Updated successfully");
+                            loaddata();
+
+                        }
+                    }
+                }
+            }
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string connString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Admin\OneDrive\Documents\10012884\Database51.accdb";
+            DataTable dataTableRes = new DataTable();
+            using (OleDbConnection conn = new OleDbConnection(connString))
+            {
+                OleDbCommand cmd = new OleDbCommand("SELECT * FROM Table1 where regno=@reg", conn);
+
+                conn.Open();
+                cmd.Parameters.AddWithValue("@national", txtBoxSearch.Text);
+                OleDbDataAdapter adapter = new OleDbDataAdapter(cmd);
+
+                adapter.Fill(dataTableRes);
+            }
+
+            dataGridView1.DataSource = dataTableRes;
+
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Refresh();
+            loaddata();
+        }
+
+        private void dataGridView1_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+                txtBoxNational.Text = row.Cells[0].Value.ToString();
+                txtBoxFirst.Text = row.Cells[1].Value.ToString();
+                txtBoxOther.Text = row.Cells[2].Value.ToString();
+                comboBoxId.Text = row.Cells[3].Value.ToString();
+                txtBoxWork.Text = row.Cells[4].Value.ToString();
+                txtBoxCompe.Text = row.Cells[4].Value.ToString();
+
+            }
+
+
+        }
+        public void cleartextboxes(Control.ControlCollection ctlcollection)
+        {
+            foreach (Control txt in ctlcollection)
+            {
+                if (txt is TextBoxBase)
+                {
+                    txt.Text = string.Empty;
+                }
+                else
+                {
+                    cleartextboxes(txt.Controls);
+                }
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            cleartextboxes(this.Controls);
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            txtBoxNational.Clear();
+            txtBoxFirst.Clear();
+            txtBoxOther.Clear();            
+            txtBoxWork.Clear();
+            txtBoxCompe.Clear();
+        }
+
+        private void dataGridView1_RowPrePaint(object sender, DataGridViewRowPrePaintEventArgs e)
+        {
+            
+        }
+    }
+}
+
